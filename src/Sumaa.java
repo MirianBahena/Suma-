c58ee13c8d596ecfdd5aee4b4aa5b170f923858b
Mirian Bahena <104159031+ArtBaMa22@users.noreply.github.com>
2023-02-09 07:19:03 -0600

    Practica1 Terminada -Mirian Bahena

---
+++ src/Sumaa.java
@@ -0,0 +1,10 @@
+public class Sumaa {
+    public static void main(String[] args) {
+
+        int num1 = 20;
+        int num2 = 30;
+
+        int resultado = num1+num2;
+        System.out.println("El resultado de la suma es: "+resultado);
+    }
+}
